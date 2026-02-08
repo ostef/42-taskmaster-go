efc212336e491d37488cb0cf475250bde9f0586d
Stefan <oumansour.stefan@gmail.com>
2026-02-08 13:33:02 +0100

    Basic process handling (starting, start grace period, shutting down, killing)

--- src/main.go
+++ src/main.go
@@ -1,7 +1,97 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"context"
+	"os"
+	"os/exec"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+type ProcessStatus uint
+const (
+	ProcessStatusStarted  = iota
+	ProcessStatusRunning  = iota
+	ProcessStatusStopping = iota
+	ProcessStatusStopped  = iota
+)
+
+func runProcess(ctx context.Context, command string, args ...string) error {
+	cmd := exec.CommandContext(ctx, command, args...)
+
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	fmt.Println("Starting", command)
+
+	cmd.Start()
+
+	signalCh := make(chan os.Signal, 1)
+	signal.Notify(signalCh, os.Interrupt, syscall.SIGTERM)
+
+	status := ProcessStatusStarted
+	_ = status
+
+	doneCh := make(chan error, 1)
+
+	go func() {
+		doneCh <- cmd.Wait()
+	}()
+
+	select {
+	case <-signalCh:
+		fmt.Println("Shutting down process...")
+		_ = cmd.Process.Signal(syscall.SIGTERM)
+
+		select {
+		case err := <-doneCh:
+			fmt.Println("Process exited gracefully:", err)
+			return err;
+
+		case <-time.After(2 * time.Second):
+			fmt.Println("Process still exiting, sending SIGKILL...")
+			_ = cmd.Process.Kill()
+			err := <-doneCh
+			fmt.Println("Process killed:", err)
+
+			return err
+		}
+
+	case err := <-doneCh:
+		fmt.Println("Process exited early:", err)
+		return err
+
+	case <-time.After(2 * time.Second):
+		status = ProcessStatusRunning
+		fmt.Println("Process has sucessfully started")
+	}
+
+	return <-doneCh
+}
 
 func main() {
-	fmt.Println("Hello Sailor!")
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	// sigCh := make(chan os.Signal, 1)
+	// signal.Notify(sigCh, os.Interrupt, syscall.SIGTERM)
+
+	// go func() {
+	// 	<-sigCh
+	// 	fmt.Println("\nShutting down...")
+	// 	cancel()
+	// }()
+
+	for {
+		err := runProcess(ctx, "sleep", "3")
+
+		if err != nil {
+			return
+		}
+
+		fmt.Println("Restarting in 1 second...")
+		time.Sleep(1 * time.Second)
+	}
 }
